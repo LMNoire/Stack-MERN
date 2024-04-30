ffa69e7be8abf871ebc7a9103ded3013cfa434a0
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-04-30 13:43:29 +0200

    Ajout Notes app initialisation composants

--- Notes app/frontend/notes-app/src/utils/helper.js
+++
@@ -1,4 +0,0 @@
-export const validateEmail = (email) => {
-    const regex = /^[^\s@]+@[^z@]+\.[^\s@]+$/;
-    return regex.test(email);
-};
--- Notes app/frontend/notes-app/tailwind.config.js
+++ Notes app/frontend/notes-app/tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#2B85FF",
-        secondary: "#EF863E",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
