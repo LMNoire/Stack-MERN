7ba8b7e44fbf0b5e19a978aea0da97038737a838
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-05 08:20:48 +0200

    Update Notes app config backend Nodejs

---
+++ Notes app/backend/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ data: "hello" });
+});
+
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, Message: "Full name is required" });
+  }
+  if (!email) {
+    return res.status(400).json({ error: true, Message: "Email is required" });
+  }
+  if (!password) {
+    return res.status(400).json({ error: true, Message: "Password is required"});
+  }
+
+});
+
+app.listen(3001);
+
+module.exports = app;
---
+++ Notes app/backend/models/user.model.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema({
+  fullName: { type: String },
+  email: { type: String },
+  createdOn: { type: Date, default: new Date().getTime() },
+});
+
+module.exports = mongoose.model("User", userSchema);
