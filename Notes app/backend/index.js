6792dade68988503c7770383b9ffaa5161df1073
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-05 09:45:28 +0200

    Updates Notes app methode post login backend nodejs

--- Notes app/backend/index.js
+++ Notes app/backend/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const bcrypt = require("bcryptjs");
+
 const config = require("./config.json");
 const mongoose = require("mongoose");
 
@@ -24,45 +26,72 @@ app.get("/", (req, res) => {
 app.post("/create-account", async (req, res) => {
   const { fullName, email, password } = req.body;
 
-  if (!fullName) {
+  if (!fullName || !email || !password) {
     return res
       .status(400)
-      .json({ error: true, Message: "Full name is required" });
-  }
-  if (!email) {
-    return res.status(400).json({ error: true, Message: "Email is required" });
-  }
-  if (!password) {
-    return res
-      .status(400)
-      .json({ error: true, Message: "Password is required" });
+      .json({
+        error: true,
+        message: "Full name, email, and password are required",
+      });
   }
 
   const isUser = await User.findOne({ email: email });
   if (isUser) {
-    return res.json({
-      error: true,
-      Message: "User already exist",
-    });
+    return res.json({ error: true, message: "User already exists" });
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = new User({
     fullName,
     email,
-    password,
+    password: hashedPassword,
   });
   await user.save();
-
-  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "30m"
-  })
 
   return res.json({
     error: false,
     user,
-    accessToken,
-    message: "Registration successful"
-  })
+    message: "Registration successful",
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (!userInfo.password) {
+    return res.status(400).json({ message: "User password not found" });
+  }
+
+  const validPassword = await bcrypt.compare(password, userInfo.password);
+
+  if (validPassword) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "30m",
+    });
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid Credentials",
+    });
+  }
 });
 
 app.listen(3001);
--- Notes app/backend/models/user.model.js
+++ Notes app/backend/models/user.model.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({ 
-  fullName: { type: String },
-  email: { type: String },
-  createdOn: { type: Date, default: Date.now }
-});
+    fullName: { type: String },
+    email: { type: String },
+    password: { type: String },
+    createdOn: { type: Date, default: Date.now }
+  });
 
 module.exports = mongoose.model("User", userSchema);
