642af76fdf639648999dc6c1697b9f9bce1d1362
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-06 08:19:55 +0200

    [Update] Notes app/Ajout endpoint Delete Notes

--- Notes app/backend/index.js
+++ Notes app/backend/index.js
@@ -189,6 +189,30 @@ app.get("/get-all-notes/", authenticateToken, async (req, res) => {
   }
 });
 
+app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { user } = req.user;
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, Message: "Note not found" });
+    }
+
+    await Note.deleteOne({ _id: noteId, userId: user._id });
+    return res.json({
+      error: false,
+      Message: "Note deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      Message: "Internal Server Error",
+    });
+  }
+});
+
 app.listen(3001);
 
 module.exports = app;
