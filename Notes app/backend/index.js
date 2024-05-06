a3a810e7df7f22c42e56c81b26114a24bb9b5570
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-06 07:59:42 +0200

    [Update] Notes app/ Endpoint get all notes

--- Notes app/backend/index.js
+++ Notes app/backend/index.js
@@ -171,6 +171,24 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
   }
 });
 
+app.get("/get-all-notes/", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+
+  try {
+    const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 });
+    return res.json({
+      error: false,
+      notes,
+      Message: "All notes retrieved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      Message: "Internal Server Error",
+    });
+  }
+});
+
 app.listen(3001);
 
 module.exports = app;
