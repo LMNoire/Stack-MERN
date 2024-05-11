815fb032abaf88d2164d263652147c752dd6c40d
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-11 10:03:49 +0200

    [Add] Notes app/ Composant EmptyCard

--- Notes app/frontend/notes-app/src/pages/Home/Home.jsx
+++ Notes app/frontend/notes-app/src/pages/Home/Home.jsx
@@ -7,6 +7,8 @@ import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosinstance";
 import Toast from "../../components/ToastMessage/Toast";
+import EmptyCard from "../../components/EmptyCard/EmptyCard";
+import AddNotesImg from "../../assets/Images/add-notes.png"
 
 Modal.setAppElement("#root");
 
@@ -36,10 +38,10 @@ const Home = () => {
     setShowToastMsg({
       isShown: true,
       message,
-      type
+      type,
     });
   };
-  
+
   const handleCloseToast = () => {
     setShowToastMsg({
       isShown: false,
@@ -74,12 +76,12 @@ const Home = () => {
   };
 
   const deleteNote = async (data) => {
-    const noteId = data._id
+    const noteId = data._id;
     try {
       const response = await axiosInstance.delete("/delete-note/" + noteId);
 
       if (response.data && !response.data.error) {
-        showToastMessage("Note Deleted Successfully", 'delete')
+        showToastMessage("Note Deleted Successfully", "delete");
         getAllNotes();
       }
     } catch (error) {
@@ -91,7 +93,7 @@ const Home = () => {
         console.log("An unexpected error occured. Please try again");
       }
     }
-  }
+  };
 
   useEffect(() => {
     getUserInfo();
@@ -104,21 +106,25 @@ const Home = () => {
     <>
       <Navbar userInfo={userInfo} />
       <div className="container mx-auto">
-        <div className="grid grid-cols-3 gap-4 mt-8 px-5">
-          {allNotes.map((item, index) => (
-            <NoteCard
-              key={item._id}
-              title={item.title}
-              date={item.createdOn}
-              content={item.content}
-              tags={item.tags}
-              isPinned={item.isPinned}
-              onEdit={() => handleEdit(item)}
-              onDelete={() => deleteNote(item)}
-              onPinNote={() => {}}
-            />
-          ))}
-        </div>
+        {allNotes.length > 0 ? (
+          <div className="grid grid-cols-3 gap-4 mt-8 px-5">
+            {allNotes.map((item, index) => (
+              <NoteCard
+                key={item._id}
+                title={item.title}
+                date={item.createdOn}
+                content={item.content}
+                tags={item.tags}
+                isPinned={item.isPinned}
+                onEdit={() => handleEdit(item)}
+                onDelete={() => deleteNote(item)}
+                onPinNote={() => {}}
+              />
+            ))}
+          </div>
+        ) : (
+          <EmptyCard imgSrc={AddNotesImg} message={`Start creating your first note`}/>
+        )}
       </div>
 
       <button
@@ -132,7 +138,9 @@ const Home = () => {
 
       <Modal
         isOpen={openAddEditModal.isShown}
-        onRequestClose={() => setOpenAddEditModal({ isShown: false, type: "add", data: null })}
+        onRequestClose={() =>
+          setOpenAddEditModal({ isShown: false, type: "add", data: null })
+        }
         style={{
           overlay: {
             backgroundColor: "rgba(0,0,0,0.2)",
