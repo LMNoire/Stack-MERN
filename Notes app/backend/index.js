08675ef99c13116f1264887f43552ca5a1e3b7fe
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-12 10:02:54 +0200

    [Add] Notes app/ Search note API

--- Notes app/backend/index.js
+++ Notes app/backend/index.js
@@ -209,9 +209,9 @@ app.get("/get-all-notes", authenticateToken, async (req, res) => {
   try {
     const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 });
 
-    const sanitizedNotes = notes.map(note => ({
+    const sanitizedNotes = notes.map((note) => ({
       ...note.toObject(),
-      tags: note.tags || [] 
+      tags: note.tags || [],
     }));
 
     return res.json({
@@ -278,6 +278,38 @@ app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
     return res.status(500).json({
       error: true,
       Message: "Internal Server Error",
+    });
+  }
+});
+
+app.get("/search-notes/", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  const { query } = req.query;
+
+  if (!query) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Search query is required" });
+  }
+
+  try {
+    const matchingNotes = await Note.find({
+      userId: user._id,
+      $or: [
+        { title: { $regex: new RegExp(query, "i") } },
+        { content: { $regex: new RegExp(query, "i") } },
+      ],
+    });
+
+    return res.json({
+      error: false,
+      notes: matchingNotes,
+      message: "Notes matching the search query retrieved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error",
     });
   }
 });
