17c772950cbc7a324087f56f2b2d2a644af40ef7
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-28 09:54:22 +0200

    [Update] Finance tracker/ Ajout pages auth et dashboard

--- Finance tracker/client/src/main.tsx
+++ Finance tracker/client/src/main.tsx
@@ -4,18 +4,18 @@ import App from './App.tsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-//Set Clerk key in a const
+// Set Clerk key in a const
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-//Error if no key
+// Error if no key
 if (!PUBLISHABLE_KEY) {
-  throw new Error ("Missing Publishable Key");
+  throw new Error("Missing Publishable Key");
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* All components inside ClerkProvider */}
-    <ClerkProvider publishableKey={}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </React.StrictMode>,
