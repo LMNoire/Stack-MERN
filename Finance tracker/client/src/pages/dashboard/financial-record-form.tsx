76341b0b0ee495abb696ced78a15e2402ad7f8bd
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-29 11:17:45 +0200

    [Update] Financial tracker/ Formulaire financial record

--- Finance tracker/client/src/pages/dashboard/financial-record-form.tsx
+++ Finance tracker/client/src/pages/dashboard/financial-record-form.tsx
@@ -1,6 +1,39 @@
 export const FinancialRecordForm = () => {
 
-    return <div>
-        Record Form
+    return <div className="form-container">
+        <form>
+            <div className="form-field">
+                <label>Description :</label>
+                <input type="text" required className="input" />
+            </div>
+            <div className="form-field">
+                <label>Amount :</label>
+                <input type="number" required className="input" />
+            </div>
+            <div className="form-field">
+                <label>Category :</label>
+                <select required className="input">
+                    <option value=""></option>
+                    <option value="Food"></option>
+                    <option value="Rent"></option>
+                    <option value="Salary"></option>
+                    <option value="Utilities"></option>
+                    <option value="Entertainment"></option>
+                    <option value="Other"></option>
+                </select>
+            </div>
+            <div className="form-field">
+                <label>Payment Method :</label>
+                <select required className="input">
+                    <option value=""></option>
+                    <option value="Credit Card"></option>
+                    <option value="Cash"></option>
+                    <option value="Bank Transfer"></option>
+                </select>
+            </div>
+            <button type="submit" className="button">
+                Add Record
+            </button>
+        </form>
     </div>
 }
