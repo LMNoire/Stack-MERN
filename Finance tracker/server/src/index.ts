cf06a882c4989848b311c271813e4fc7c4fe4854
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-01 11:49:17 +0200

    [Update] Financial tracker/ Ajout middleware

--- Finance tracker/server/src/index.ts
+++ Finance tracker/server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
+import FinancialRecordRouter from "./routes/financial-records";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
@@ -13,6 +14,8 @@ mongoose
   .connect(mongoURI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to mongoDB :", err));
+
+app.use("/financial-records", FinancialRecordRouter)
 
 app.listen(port, () => {
   console.log(`Server Running on Port ${port}`);
