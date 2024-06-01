8ede5e4fd3cd29c9e3e5becdcc46f26ee1c8faa9
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-01 11:58:20 +0200

    [Update] Financial tracker/ Ajout route post record

--- Finance tracker/server/src/routes/financial-records.ts
+++ Finance tracker/server/src/routes/financial-records.ts
@@ -10,9 +10,20 @@ router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
       userId: userId,
     });
     if (records.length === 0) {
-        return res.status(404).send("No records found for the user.")
+      return res.status(404).send("No records found for the user.");
     }
     res.status(200).send(records);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const newRecordBody = req.body;
+    const newRecord = new FinancialRecordModel(newRecordBody);
+    const savedRecord = await newRecord.save();
+    res.status(200).send(savedRecord);
   } catch (err) {
     res.status(500).send(err);
   }
