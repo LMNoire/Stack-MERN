9b0692d870f709526eea670d9ab3f4179c3cf056
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-07 15:44:25 +0200

    [Update] Financial tracker/ Suite config server

--- Finance tracker/server/src/index.ts
+++ Finance tracker/server/src/index.ts
@@ -1,2 +1,15 @@
-import express from "express";
+import express, { Express } from "express";
 import mongoose from "mongoose";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+const mongoURI: string =
+  "mongodb+srv://selimcoulombel:6b2u0BKkSF3stvAk@personalfinancetracker.lf9g6ha.mongodb.net/";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error());
