2c4e279091068e2d3f43878a201b587cb5fff97a
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-08 09:24:09 +0200

    [Update] Financial traker/ Ajout schema financial record model

--- Finance tracker/server/src/index.ts
+++ Finance tracker/server/src/index.ts
@@ -1,15 +1,23 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
 
+//Use express and define port
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
+//Mongo connexion
 const mongoURI: string =
   "mongodb+srv://selimcoulombel:6b2u0BKkSF3stvAk@personalfinancetracker.lf9g6ha.mongodb.net/";
 
+//Init mongoose
 mongoose
   .connect(mongoURI)
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error());
+  .catch((err) => console.error("Failed to connect to MongoDB :", err));
+
+//Liston to port  
+app.listen(port, () => {
+  console.log(`Server Running on Port 3001 ${port}`);
+});
