079a919154ffe20ee5229000c44dac913d262f60
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-17 09:18:36 +0200

    [Update] Financial tracker/ Suite financial record context

--- Finance tracker/client/src/pages/dashboard/financial-record-list.tsx
+++ Finance tracker/client/src/pages/dashboard/financial-record-list.tsx
@@ -1,5 +1,9 @@
+import { useFinancialRecords } from "../../contexts/financial-record-context"
+
 //List to see your records
 export const FinancialRecordlist = () => {
 
+    const { records } = useFinancialRecords();
+    
     return <div>Record List</div>
 }
