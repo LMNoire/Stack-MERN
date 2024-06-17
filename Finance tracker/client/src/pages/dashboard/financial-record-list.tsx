4d0fdd448cf7f71976d1a33c0fbf6a009b0add46
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-17 09:50:37 +0200

    [Update] Financial tracker/ Suite Financial record list

--- Finance tracker/client/src/pages/dashboard/financial-record-list.tsx
+++ Finance tracker/client/src/pages/dashboard/financial-record-list.tsx
@@ -1,12 +1,20 @@
 import { FinancialRecord, useFinancialRecords } from "../../contexts/financial-record-context";
 import { useTable, Column, CellProps, Row } from "react-table";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 interface EditableCellProps extends CellProps<FinancialRecord> {
-    
+    updateRecord: (rowIndex: number, columnId: string, value: any) => void;
+    editable: boolean;
 }
 
-const EditableCell: React.FC<EditableCellProps>
+const EditableCell: React.FC<EditableCellProps> = ({value: initialValue, row, column, updateRecord, editable}) => {
+    const [isEditing, setIsEditing] = useState(false)
+    const [value, setValue] = useState(initialValue)
+    return (
+        <div>
+            {isEditing ? <input value={value} onChange={(e) => setValue(e.target.value)} autoFocus style={{width: "100%"}} /> : typeof value === "string" ? (value) : (value.toString())}
+        </div>)
+}
 
 //List to see your records
 export const FinancialRecordlist = () => {
