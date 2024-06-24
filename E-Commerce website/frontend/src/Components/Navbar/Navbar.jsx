398eff7f2b0f8579913fd2d5e7db3d35b1fad44d
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-24 16:29:21 +0200

    [Update] E-Commerce website/ Init pages et ajout nouvelle font

--- E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
+++ E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
@@ -21,28 +21,28 @@ const Navbar = () => {
             setMenu("shop");
           }}
         >
-          Shop {menu==="shop" ? <hr />:<></>}
+          Shop {menu === "shop" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("men");
           }}
         >
-          Men {menu==="men" ? <hr />:<></>}
+          Men {menu === "men" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("women");
           }}
         >
-          Women {menu==="women" ? <hr />:<></>}
+          Women {menu === "women" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          Kids {menu==="kids" ? <hr />:<></>}
+          Kids {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
       {/* Login button */}
