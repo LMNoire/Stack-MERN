de5fc13d6d660eaa04f4545b71176c5cc912169f
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-24 16:23:45 +0200

    [Update] E-Commerce website/ Suite navbar et css

--- E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
+++ E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-      
-    </div>
-  )
-}
+  const [menu, setMenu] = useState("shop");
 
-export default Navbar
+  return (
+    //Navbar div
+    <div className="navbar">
+      {/* Logo and title */}
+      <div className="nav-logo">
+        <img src={logo} alt="A shopping bag, the logo of the company" />
+        <p>SHOPPER</p>
+      </div>
+      {/* Navbar links list */}
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop {menu==="shop" ? <hr />:<></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          Men {menu==="men" ? <hr />:<></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          Women {menu==="women" ? <hr />:<></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids {menu==="kids" ? <hr />:<></>}
+        </li>
+      </ul>
+      {/* Login button */}
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="A shopping cart" />
+        {/* Shopping cart counter */}
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
