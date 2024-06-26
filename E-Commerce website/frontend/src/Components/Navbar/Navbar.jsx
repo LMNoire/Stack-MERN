00eff4c7753d386731777f85436d6354c7884c1e
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-06-26 09:45:59 +0200

    [Update] E-Commerce website/ Ajout des link dans composant navbar

--- E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
+++ E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
@@ -21,34 +22,43 @@ const Navbar = () => {
             setMenu("shop");
           }}
         >
-          Shop {menu === "shop" ? <hr /> : <></>}
+          <Link to="/">Shop</Link>
+          {menu === "shop" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("men");
           }}
         >
-          Men {menu === "men" ? <hr /> : <></>}
+          <Link to="/mens">Men</Link>
+          {menu === "men" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("women");
           }}
         >
-          Women {menu === "women" ? <hr /> : <></>}
+          <Link to="/womens">Women</Link>
+          {menu === "women" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          Kids {menu === "kids" ? <hr /> : <></>}
+          <Link to="/kids">Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
+
       {/* Login button */}
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="A shopping cart" />
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="A shopping cart" />
+        </Link>
         {/* Shopping cart counter */}
         <div className="nav-cart-count">0</div>
       </div>
