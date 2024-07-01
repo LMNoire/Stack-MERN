23ad22b29b24e2c04927de9e72c9c9a82efab51c
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-07-01 11:33:20 +0200

    [Update] E-Commerce website/ Ajout CSS DescriptionBox

--- E-Commerce website/frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ E-Commerce website/frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -7,23 +7,23 @@ const DescriptionBox = () => {
       <div className="descriptionbox-navigator">
         <div className="descriptionbox-nav-box">Description</div>
         <div className="descriptionbox-nav-box fade">Reviews (122)</div>
-        <div className="descriptionbox-description">
-          <p>
-            An e-commerce website is an online platform that facilitate buying
-            and selling of products or services over the internet and serves as
-            a virtual marketplace where businesses and individuals can showcase
-            their products, interact with customers, and conduct transactions
-            without the need for a physical presence. E-commerce websites have
-            gained immense popularity due to their convenience, accessibility,
-            and the global reach they offer.
-          </p>
-          <p>
-            E-commerce websites typically display products or services with
-            detailed descriptions, images, prices, and any available information
-            (e.g., sizes, colors). Each product usually has its own dedicated
-            page with relevant information.
-          </p>
-        </div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          An e-commerce website is an online platform that facilitate buying and
+          selling of products or services over the internet and serves as a
+          virtual marketplace where businesses and individuals can showcase
+          their products, interact with customers, and conduct transactions
+          without the need for a physical presence. E-commerce websites have
+          gained immense popularity due to their convenience, accessibility, and
+          the global reach they offer.
+        </p>
+        <p>
+          E-commerce websites typically display products or services with
+          detailed descriptions, images, prices, and any available information
+          (e.g., sizes, colors). Each product usually has its own dedicated page
+          with relevant information.
+        </p>
       </div>
     </div>
   );
