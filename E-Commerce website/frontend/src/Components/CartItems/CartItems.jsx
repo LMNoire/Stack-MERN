85f0527a1e83d6e6214d41f017b9109b374470d4
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-07-02 10:05:19 +0200

    [Update] E-Commerce website/ Ajout Component CartItems

--- E-Commerce website/frontend/src/Components/CartItems/CartItems.jsx
+++ E-Commerce website/frontend/src/Components/CartItems/CartItems.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "./CartItems.css";
 import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItems = () => {
   const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
@@ -16,16 +17,30 @@ const CartItems = () => {
         <p>Remove</p>
       </div>
       <hr />
-      <div>
-        <div className="cartitems-format">
-          <img src="" alt="" className="carticon-product-icon" />
-          <p></p>
-          <p></p>
-          <button className="cartitems-quantity"></button>
-          <p></p>
-          <img src="" alt="" />
-        </div>
-      </div>
+      {all_product.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartitems-format">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
+                <p>{e.new_price * cartItems[e.id]}</p>
+                <img
+                  src={remove_icon}
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                />
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
