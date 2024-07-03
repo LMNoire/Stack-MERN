7434ac42bf70aa85126cea37c6a4c1db6dd06a42
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-07-03 11:44:06 +0200

    [Update] E-Commerce website/ Ajout media queries et menu navbar déroulant sous 800px

--- E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
+++ E-Commerce website/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from '../Assets/nav_dropdown.png';
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-  const { getTotalCartItems } = useContext(ShopContext) ;
+  const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     //Navbar div
@@ -17,8 +24,9 @@ const Navbar = () => {
         <img src={logo} alt="A shopping bag, the logo of the company" />
         <p>SHOPPER</p>
       </div>
+      <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       {/* Navbar links list */}
-      <ul className="nav-menu">
+      <ul ref={menuRef} className="nav-menu">
         <li
           onClick={() => {
             setMenu("shop");
