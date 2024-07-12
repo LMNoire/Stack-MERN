227012e596f50fb88f5550f4b1d03675ae7cd178
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-07-12 10:55:02 +0200

    [Update] E-Commerce website/ Configuration initialisation backend

--- E-Commerce website/backend/index.js
+++ E-Commerce website/backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: ".env.local" });
 const port = 4000;
 const express = require("express");
 const app = express();
@@ -6,7 +7,23 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { error } = require("console");
 
 app.use(express.json());
 app.use(cors());
 
+//MongoDB Connexion
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+//Main path
+app.get("/", (req, res) => {
+  res.send("Express App is running");
+});
+
+//Running server
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
