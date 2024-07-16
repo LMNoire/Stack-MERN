6d1aa016855f41d34fe51180173c7946d181c5eb
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-07-16 19:16:54 +0200

    [Update] E-Commerce website/ Ajout Component Navbar pour Admin

--- E-Commerce website/admin/src/App.jsx
+++ E-Commerce website/admin/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
