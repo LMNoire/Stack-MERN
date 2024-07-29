1e6759115b4dceb59b84ef48a55d7152ab131926
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-07-29 10:28:56 +0200

    [Update] E-Commerce website/ Ajout fonction Login et Signup dans partie frontend

--- E-Commerce website/frontend/src/Pages/LoginSignup.jsx
+++ E-Commerce website/frontend/src/Pages/LoginSignup.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Your Name" />
+          {state === "Sign Up" ? (
+            <input type="text" placeholder="Your Name" />
+          ) : (
+            <></>
+          )}
           <input type="email" placeholder="Email Address" />
           <input type="password" placeholder="Password" />
         </div>
         <button>Continue</button>
-        <p className="loginsignup-login">
-          Already have an account ? <span>Login here</span>
-        </p>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account ? <span>Login here</span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account <span>Click here</span>
+          </p>
+        )}
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
-          <p>By continuing, i agree to the termes of use & privacy policy.</p>
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
         </div>
       </div>
     </div>
