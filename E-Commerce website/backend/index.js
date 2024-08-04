4ceddd9758661b05371d8159972ab1b5a1b3ade6
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-04 09:25:35 +0200

    [Update] E-Commerce website/ Ajout middelware index partie frontend

--- E-Commerce website/backend/index.js
+++ E-Commerce website/backend/index.js
@@ -216,6 +216,29 @@ app.get("/popularinwomen", async (req, res) => {
   res.send(popular_in_women);
 });
 
+//Middelware to fetch user
+const fetchUser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  } else {
+    try {
+      const data = jwt.verify(token, "secret_ecom");
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res
+        .status(401)
+        .send({ errors: "Please authenticate using a valid token" });
+    }
+  }
+};
+
+//Endpoint for adding product in cartdata
+app.post("/addtocart", fetchUser, async (req, res) => {
+  console.log(req.body, req.user);
+});
+
 //Running server
 app.listen(port, () => {
   console.log("Server running on port " + port);
