13a605e33108d0b5bcaea0db3f9917e1379cb98d
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-04 09:48:37 +0200

    [Update] E-Commerce website/ Ajout endpoint getCart dans index partie backend

--- E-Commerce website/backend/index.js
+++ E-Commerce website/backend/index.js
@@ -259,6 +259,13 @@ app.post("/removefromcart", fetchUser, async (req, res) => {
   res.send("Removed");
 });
 
+//Endpoint to get cartdata
+app.post("/getcart", fetchUser, async (req, res) => {
+  console.log("Get cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  res.json(userData.cartData);
+});
+
 //Running server
 app.listen(port, () => {
   console.log("Server running on port " + port);
