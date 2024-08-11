c33a81dc1778fbb730bc2cb8f6390eeb45d0f567
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-11 10:22:20 +0200

    [Update] Podcaster/ Ajout route Podcast dans app.js backend

--- Podcaster audio platform/backend/app.js
+++ Podcaster audio platform/backend/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const userApi = require ("./routes/user.js");
 const CatApi = require ("./routes/categories.js");
+const PodcastApi = require ("./routes/podcast.js");
 const cookieParser = require("cookie-parser");
+
 
 require("dotenv").config();
 require("./conn/conn");
@@ -10,8 +12,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Routes
-app.use("/api/v1", userApi)
-app.use("/api/v1", CatApi)
+app.use("/api/v1", userApi);
+app.use("/api/v1", CatApi);
+app.use("/api/v1", PodcastApi);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port : ${process.env.PORT}`);
