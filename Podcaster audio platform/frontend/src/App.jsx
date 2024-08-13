739477d5f87a0329d908cd8a65a1ddf0737d50b3
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-13 19:42:19 +0200

    [Update] Podcaster/ Initialisation Components, Pages et Layout pour frontend

--- Podcaster audio platform/frontend/src/App.jsx
+++ Podcaster audio platform/frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
 
 function App() {
-  
-
   return (
-    <div className='text-red-500'></div>
-  )
+    <div className="">
+      <Router>
+        <Routes path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
