1fe2ea16512d159465da70cb551801707f6af6cb
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-17 11:19:09 +0200

    [Update] Gym website/ Ajout suite Component Contact pour frontend

--- Gym website/frontend/src/components/Contact.jsx
+++ Gym website/frontend/src/components/Contact.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+import { ClipLoader } from "react-spinners";
+
 const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState("");
+  const [loading] = useState(false);
+
   return (
     <section className="contact">
       <form>
@@ -17,13 +21,33 @@ const Contact = () => {
         </div>
         <div>
           <label>Email</label>
-          <input type="email" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label>Message</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
-        <button type="submit">Send Message</button>
+        <button
+          type="submit"
+          disabled={loading}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "15px",
+          }}
+        >
+          {loading && <ClipLoader size={20} color="white" />}
+          Send Message
+        </button>
       </form>
     </section>
   );
