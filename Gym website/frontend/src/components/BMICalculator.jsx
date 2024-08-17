4947d7b040ab0ae132b2b3d68e925d11758a8529
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-17 11:23:58 +0200

    [Update] Gym website/ Ajout Component BMICalculator pour frontend

--- Gym website/frontend/src/components/BMICalculator.jsx
+++ Gym website/frontend/src/components/BMICalculator.jsx
@@ -1,11 +1,25 @@
-
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const BMICalculator = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [gender, setGender] = useState("");
+  const [bmi, setBmi] = useState("");
 
-export default BMICalculator
+  const calculateBmi = (e) => {
+    e.preventDefault();
+
+    if (!height || !weight || !gender) {
+      toast.error("Please enter valid height, weight and gender.");
+      return;
+    }
+
+    const heightInMeters = height / 100;
+    const bmiValue = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+  };
+
+  return <></>;
+};
+
+export default BMICalculator;
