1bdcbe7a194dd362de967c2ed02a91ff571bc949
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-17 11:12:01 +0200

    [Update] Gym website/ Ajout Component Contact pour frontend

--- Gym website/frontend/src/components/Contact.jsx
+++ Gym website/frontend/src/components/Contact.jsx
@@ -1,11 +1,32 @@
-
-
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState("");
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="contact">
+      <form>
+        <h1>CONTACT US</h1>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>Message</label>
+          <input type="text" />
+        </div>
+        <button type="submit">Send Message</button>
+      </form>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
