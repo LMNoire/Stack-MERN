e65d5592729a028eb4a9fc1e73d9da324576099b
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-28 11:18:51 +0200

    [Update] Chat app/ Suite Component Login

--- Chat app/src/pages/Login/Login.jsx
+++ Chat app/src/pages/Login/Login.jsx
@@ -1,11 +1,15 @@
-import './Login.css';
+import "./Login.css";
+import assets from "../../assets/assets";
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className="login">
+      <img src={assets.logo_big} alt="" className="logo" />
+      <form className="login-form">
+        <h2>Sign Up</h2>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
