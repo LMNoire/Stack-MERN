6f06bd61e60b3da3afc023e9c2d00ddb36bd2b8f
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-28 11:12:22 +0200

    [Ajout] Chat app/ Suite initialisation Components, Pages et ajout Routes

--- Chat app/src/App.jsx
+++ Chat app/src/App.jsx
@@ -1,11 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login/login";
+import Chat from "./pages/chat/chat";
+import ProfilteUpdate from "./pages/profileUpdate/profilteUpdate";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<ProfilteUpdate />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ Chat app/src/pages/ProfileUpdate/ProfilteUpdate.jsx
@@ -0,0 +1,11 @@
+import './ProfileUpdate.css';
+
+const ProfilteUpdate = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfilteUpdate
