e94b58c9cf6c0760a2da8ac3ac0c034113b8dd18
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-29 12:36:11 +0200

    [Update] Chat app/ Ajout toggle entre Signup et Login sur Page Login

--- Chat app/src/pages/Login/Login.jsx
+++ Chat app/src/pages/Login/Login.jsx
@@ -1,18 +1,23 @@
 import "./Login.css";
 import assets from "../../assets/assets";
+import { useState } from "react";
 
 const Login = () => {
+  const [currState, setCurrState] = useState("Sign up");
+
   return (
     <div className="login">
       <img src={assets.logo_big} alt="" className="logo" />
       <form className="login-form">
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="username"
-          className="form-input"
-          required
-        />
+        <h2>{currState}</h2>
+        {currState === "Sign up" ? (
+          <input
+            type="text"
+            placeholder="username"
+            className="form-input"
+            required
+          />
+        ) : null}
         <input
           type="email"
           placeholder="Email address"
@@ -25,15 +30,25 @@ const Login = () => {
           className="form-input"
           required
         />
-        <button type="submit">Sign Up</button>
+        <button type="submit">
+          {currState === "Sign up" ? "Create account" : "Login now"}
+        </button>
         <div className="login-term">
           <input type="checkbox" />
           <p>Agree to the terms of use & privacy policy.</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">
-            Already have an account ? <span>Click here</span>
-          </p>
+          {currState === "Sign up" ? (
+            <p className="login-toggle">
+              Already have an account ?{" "}
+              <span onClick={() => setCurrState("Login")}>Login here</span>
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Don&apos;t have an account ?{" "}
+              <span onClick={() => setCurrState("Sign up")}>Click here</span>
+            </p>
+          )}
         </div>
       </form>
     </div>
