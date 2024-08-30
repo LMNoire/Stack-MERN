251dca9c00d7b5757760189f04e1e02f1644a902
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-30 11:27:09 +0200

    [Update] Chat app/ Suite developpement Component Chatbox

--- Chat app/src/components/ChatBox/ChatBox.jsx
+++ Chat app/src/components/ChatBox/ChatBox.jsx
@@ -11,6 +11,24 @@ const ChatBox = () => {
         </p>
         <img src={assets.help_icon} className="help" alt="" />
       </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">Test text</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">Test text</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
       <div className="chat-input">
         <input type="text" placeholder="Send a message" />
         <input type="file" id="image" accept="image/png, image/jpeg" hidden />
