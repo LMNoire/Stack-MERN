1f7bfdea74333e6d3ee6faeb1eb6e5edc876d2af
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-30 10:45:05 +0200

    [Update] Chat app/ Développement Component LeftSidebar

--- Chat app/src/components/LeftSidebar/LeftSidebar.jsx
+++ Chat app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,11 +1,32 @@
-import './LeftSidebar.css';
+import "./LeftSidebar.css";
+import assets from "../../assets/assets";
 
 const LeftSidebar = () => {
   return (
-    <div>
-      
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder="Search here.." />
+        </div>
+      </div>
+      <div className="ls-list">
+        <div className="friends">
+          <img src={assets.profile_img} alt="" />
+          <div>
+            <p>Richard Sanford</p>
+            <span>Hello, How are you ?</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
