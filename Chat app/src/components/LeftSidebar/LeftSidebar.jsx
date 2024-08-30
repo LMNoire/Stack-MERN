2e963d1ca15129b5f06f800224338ffac81507db
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-30 11:06:07 +0200

    [Update] Chat app/ Suite Component LeftSidebar

--- Chat app/src/components/LeftSidebar/LeftSidebar.jsx
+++ Chat app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -17,7 +17,17 @@ const LeftSidebar = () => {
         </div>
       </div>
       <div className="ls-list">
-
+        {Array(12)
+          .fill("")
+          .map((item, index) => (
+            <div key={index} className="friends">
+              <img src={assets.profile_img} alt="" />
+              <div>
+                <p>Richard Sanford</p>
+                <span>Hello, How are you ?</span>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
