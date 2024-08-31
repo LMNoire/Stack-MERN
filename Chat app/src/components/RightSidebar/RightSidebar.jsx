690097285e8dc789c7f10fd89c65a76bd2fb8db4
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-08-31 11:53:27 +0200

    [Update] Chat app/ Developpement Component RightSidebar

--- Chat app/src/components/RightSidebar/RightSidebar.jsx
+++ Chat app/src/components/RightSidebar/RightSidebar.jsx
@@ -1,11 +1,31 @@
-import './RightSidebar.css';
+import "./RightSidebar.css";
+import assets from "../../assets/assets";
 
 const RightSidebar = () => {
   return (
-    <div>
-      
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          Richard Sanford <img className="dot" src={assets.green_dot} alt="" />
+        </h3>
+        <p>Hey, There i am Richard Sanford using chat app</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
     </div>
-  )
-}
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
