55025b2db28513a12fa41d4d6fd289a4900d140d
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-01 11:04:49 +0200

    [Update] Chat app/ Ajout fonction changement profile pic dans  page ProfileUpdate + correction nom page

--- Chat app/src/App.jsx
+++ Chat app/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login/login";
 import Chat from "./pages/chat/chat";
-import ProfilteUpdate from "./pages/profileUpdate/profilteUpdate";
+import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
-        <Route path="/profile" element={<ProfilteUpdate />} />
+        <Route path="/profile" element={<ProfileUpdate />} />
       </Routes>
     </>
   );
