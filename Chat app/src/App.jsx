cc757d5f01af6778446da182ae545930340f04e4
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-08 13:32:47 +0200

    [Update] Chat app/ Initialisation AppContext

--- Chat app/src/App.jsx
+++ Chat app/src/App.jsx
@@ -2,30 +2,28 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Chat from "./pages/chat/chat";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./config/firebase";
 
 const App = () => {
-
   const navigate = useNavigate();
 
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
-        navigate('/chat')
+        navigate("/chat");
+      } else {
+        navigate("/");
       }
-      else {
-        navigate('/')
-      }
-    })
-  })
+    });
+  });
 
   return (
     <>
-    <ToastContainer/>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
