320d186daf321e913fed9325961acd1168245db9
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-08 13:45:29 +0200

    [Update] Chat app/ Suite de l'AppContext

--- Chat app/src/context/AppContext.jsx
+++ Chat app/src/context/AppContext.jsx
@@ -1,23 +1,49 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import PropTypes from "prop-types";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
 
-    const value = {
+    const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
+  const [chatData, setChatData] = useState(null);
 
+  const loadUserData = async (uid) => {
+    try {
+        const userRef = doc(db, 'users', uid);
+        const userSnap = await getDoc(userRef);
+        const userData = userSnap.data();
+        setUserData(userData);
+        if (userData.avatar && userData.name) {
+            navigate('/chat');
+        }
+        else {
+            navigate('/profile')
+        }
+    } catch (error) {
+        
     }
+  }
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const value = {
+    userData,
+    setUserData,
+    chatData,
+    setChatData,
+    loadUserData
+  };
 
-AppContextProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
 };
 
-export default AppContextProvider
+AppContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppContextProvider;
