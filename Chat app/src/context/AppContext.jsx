e6c1a72a075889bf6042af0a2e0e3b121b058ff6
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-09 14:20:06 +0200

    [Update] Chat app/ Ajout fonction upload pour fichier photo

--- Chat app/src/context/AppContext.jsx
+++ Chat app/src/context/AppContext.jsx
@@ -7,44 +7,42 @@ import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
   const [chatData, setChatData] = useState(null);
 
   const loadUserData = async (uid) => {
     try {
-        const userRef = doc(db, 'users', uid);
-        const userSnap = await getDoc(userRef);
-        const userData = userSnap.data();
-        setUserData(userData);
-        if (userData.avatar && userData.name) {
-            navigate('/chat');
+      const userRef = doc(db, "users", uid);
+      const userSnap = await getDoc(userRef);
+      const userData = userSnap.data();
+      setUserData(userData);
+      if (userData.avatar && userData.name) {
+        navigate("/chat");
+      } else {
+        navigate("/profile");
+      }
+      await updateDoc(userRef, {
+        lastSeen: Date.now(),
+      });
+      setInterval(async () => {
+        if (auth.chatUser) {
+          await updateDoc(userRef, {
+            lastSeen: Date.now(),
+          });
         }
-        else {
-            navigate('/profile')
-        }
-        await updateDoc(userRef, {
-            lastSeen: Date.now()
-        })
-        setInterval(async () => {
-            if (auth.chatUser) {
-                await updateDoc(userRef, {
-                    lastSeen: Date.now()
-                }) 
-            }
-        }, 60000)
+      }, 60000);
     } catch (error) {
-        
+      console.error("Failed to load user data", error);
     }
-  }
+  };
 
   const value = {
     userData,
     setUserData,
     chatData,
     setChatData,
-    loadUserData
+    loadUserData,
   };
 
   return (
