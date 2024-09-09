1b44ba151a3392384e072858c89f2707a8eae6de
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-09 14:38:12 +0200

    [Update] Chat app/ Suite Component ProfileUpdate

--- Chat app/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ Chat app/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,16 +1,72 @@
 import "./ProfileUpdate.css";
 import assets from "../../assets/assets";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import upload from "../../lib/upload";
 
 const ProfileUpdate = () => {
+  const navigate = useNavigate();
   const [image, setImage] = useState(false);
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
+  const [uid, setUid] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+
+  const ProfileUpdate = async (event) => {
+    event.preventDefault();
+    try {
+      if (!prevImage && !image) {
+        toast.error("Upload profile picture");
+      }
+      const docRef = doc(db, "users", uid);
+      if (image) {
+        const imgUrl = await upload(image);
+        setPrevImage(imgUrl);
+        await updateDoc(docRef, {
+          avatar: imgUrl,
+          bio: bio,
+          name: name,
+        });
+      } else {
+        await updateDoc(docRef, {
+          bio: bio,
+          name: name,
+        });
+      }
+    } catch (error) {
+      console.error("Failed to load user data", error);
+    }
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUid(user.uid);
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.data().name) {
+          setName(docSnap.data().name);
+        }
+        if (docSnap.data().bio) {
+          setBio(docSnap.data().bio);
+        }
+        if (docSnap.data().avatar) {
+          setPrevImage(docSnap.data().avatar);
+        }
+      } else {
+        navigate("/");
+      }
+    });
+  });
 
   return (
     <div className="profile">
       <div className="profile-container">
-        <form>
+        <form onSubmit={ProfileUpdate}>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input
