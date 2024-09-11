1b6b6b2936a37648ef276beea3d8dc20e792c076
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-11 12:33:12 +0200

    [Update] Chat app/ Suite affichage search results dans LeftSidebar Component

--- Chat app/src/components/LeftSidebar/LeftSidebar.jsx
+++ Chat app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -4,21 +4,30 @@ import { useNavigate } from "react-router-dom";
 import { collection, getDoc, query, where } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { toast } from "react-toastify";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
   const { userData } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+  const [showSearch, setShowSearch] = useState(false);
 
   const inputHandler = async (e) => {
     try {
       const input = e.target.value;
-      const userRef = collection(db, "users");
-      const q = query(userRef, where("username", "==", input.toLowerCase()));
-      const querySnap = await getDoc(q);
-      if (!querySnap.empty && querySnap.docs[0].data.id !== userData.id) {
-        console.log(querySnap.docs[0].data());
+      if (input) {
+        setShowSearch(true);
+        const userRef = collection(db, "users");
+        const q = query(userRef, where("username", "==", input.toLowerCase()));
+        const querySnap = await getDoc(q);
+        if (!querySnap.empty && querySnap.docs[0].data.id !== userData.id) {
+          setUser(querySnap.docs[0].data);
+        } else {
+          setUser(null);
+        }
+      } else {
+        setShowSearch(false);
       }
     } catch (error) {
       console.error(error);
@@ -50,17 +59,24 @@ const LeftSidebar = () => {
         </div>
       </div>
       <div className="ls-list">
-        {Array(12)
-          .fill("")
-          .map((item, index) => (
-            <div key={index} className="friends">
-              <img src={assets.profile_img} alt="" />
-              <div>
-                <p>Richard Sanford</p>
-                <span>Hello, How are you ?</span>
+        {showSearch && user ? (
+          <div className="friends add-user">
+            <img src={user.avatar} alt="" />
+            <p>{user.name}</p>
+          </div>
+        ) : (
+          Array(12)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="friends">
+                <img src={assets.profile_img} alt="" />
+                <div>
+                  <p>Richard Sanford</p>
+                  <span>Hello, How are you ?</span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+        )}
       </div>
     </div>
   );
