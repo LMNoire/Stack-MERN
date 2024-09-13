496b0ba9167246d70a8ed6d2046fc12d5b34b117
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-13 11:55:33 +0200

    [Update] Chat app/ Suite affichage user pour Component LeftSidebar

--- Chat app/src/components/LeftSidebar/LeftSidebar.jsx
+++ Chat app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -119,17 +119,15 @@ const LeftSidebar = () => {
             <p>{user.name}</p>
           </div>
         ) : (
-          Array(12)
-            .fill("")
-            .map((item, index) => (
-              <div key={index} className="friends">
-                <img src={assets.profile_img} alt="" />
-                <div>
-                  <p>Richard Sanford</p>
-                  <span>Hello, How are you ?</span>
-                </div>
+          chatsData.map((item, index) => (
+            <div key={index} className="friends">
+              <img src={item.userData.avatar} alt="" />
+              <div>
+                <p>{item.userData.name}</p>
+                <span>{item.lastMessage}</span>
               </div>
-            ))
+            </div>
+          ))
         )}
       </div>
     </div>
