88abf85a6fa6768c4f9602aea96e9d6ee5007b5c
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-13 11:46:29 +0200

    [Update] Chat app/ Ajout update message pour Component LeftSidebar

--- Chat app/src/components/LeftSidebar/LeftSidebar.jsx
+++ Chat app/src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,7 +1,17 @@
 import "./LeftSidebar.css";
 import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
-import { collection, doc, getDoc, query, serverTimestamp, setDoc, where } from "firebase/firestore";
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { toast } from "react-toastify";
 import { useContext, useState } from "react";
@@ -40,15 +50,36 @@ const LeftSidebar = () => {
     const chatsRef = collection(db, "chats");
     try {
       const newMessageRef = doc(messagesRef);
+
       await setDoc(newMessageRef, {
         createdAt: serverTimestamp(),
-        messages: []
-      })
+        messages: [],
+      });
+
+      await updateDoc(doc(chatsRef, user.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: "",
+          rId: userData.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      });
+
+      await updateDoc(doc(chatsRef, userData.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: "",
+          rId: user.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      });
     } catch (error) {
       console.error(error);
       toast.error(error.message);
     }
-  }
+  };
 
   return (
     <div className="ls">
