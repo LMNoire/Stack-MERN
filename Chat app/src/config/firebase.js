d8313643db3946f09b8a1069bd288715bb59b3fe
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-22 13:13:46 +0200

    [Update] Chat app/ Ajout fonction reset password dans Page Login

--- Chat app/src/config/firebase.js
+++ Chat app/src/config/firebase.js
@@ -10,7 +10,6 @@ import {
 import {
   collection,
   doc,
-  getDoc,
   getDocs,
   getFirestore,
   query,
--- Chat app/src/pages/Login/Login.jsx
+++ Chat app/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import "./Login.css";
 import assets from "../../assets/assets";
 import { useState } from "react";
-import { signup, login } from "../../config/firebase";
+import { signup, login, resetPass } from "../../config/firebase";
 
 const Login = () => {
   const [currState, setCurrState] = useState("Sign up");
@@ -68,6 +68,12 @@ const Login = () => {
               <span onClick={() => setCurrState("Sign up")}>Click here</span>
             </p>
           )}
+          {currState === "Login" ? (
+            <p className="login-toggle">
+              Forgot Password ?{" "}
+              <span onClick={() => resetPass(email)}>Reset here</span>
+            </p>
+          ) : null}
         </div>
       </form>
     </div>
