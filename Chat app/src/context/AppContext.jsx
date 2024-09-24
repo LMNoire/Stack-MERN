7b56ad54268ff8997bc724c30b98b42172d4baf6
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-09-24 11:34:58 +0200

    [Update] Chat app/ Ajout fonction chatVisible dans AppContext et CSS Responsive RightSidebar

--- Chat app/src/context/AppContext.jsx
+++ Chat app/src/context/AppContext.jsx
@@ -13,6 +13,7 @@ const AppContextProvider = (props) => {
   const [messagesId, setMessagesId] = useState(null);
   const [messages, setMessages] = useState([]);
   const [chatUser, setChatUser] = useState(null);
+  const [chatVisible, setChatVisible] = useState(false);
 
   const loadUserData = async (uid) => {
     try {
@@ -72,6 +73,8 @@ const AppContextProvider = (props) => {
     setMessagesId,
     chatUser,
     setChatUser,
+    chatVisible,
+    setChatVisible,
   };
 
   return (
